e5e172b5b1c53321b37de3a49df4f443b4d6ff3f
madhavkumaryadav <ymadhav081@gmail.com>
2026-02-18 13:04:29 +0530

    Implement The Subtract Function

--- add.py
+++ add.py
@@ -3,9 +3,15 @@ def addition():
     b=int(input("Enter Second nmt3er : "))
     print(f"Addition of {a} and {b} is : ",a+b)
     
+def subtract():
+    a=int(input("Enter first number : " ))
+    b=int(input("Enter Second nmt3er : "))
+    print(f"Addition of {a} and {b} is : ",a-b)
+    
 def show():
     print("1. addition ")
-    print("2. Exit ")
+    print("2. Subtract")
+    print("3. Exit ")
     
 if __name__=='__main__':
     while True:
@@ -14,8 +20,9 @@ if __name__=='__main__':
         option=int(input("Enter your option : "))
         if option == 1:
             res=addition()
-            
-        if  option ==2:
+        elif option==2:
+            res=subtract()
+        elif  option ==3:
             break
         else:
             print("provide the valid input ......")
