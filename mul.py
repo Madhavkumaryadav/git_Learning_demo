6bf6dbae083990483b9aa2d25bad2ceccfecb736
madhavkumaryadav <ymadhav081@gmail.com>
2026-02-18 13:26:51 +0530

    sr_dev commet and update

---
+++ mul.py
@@ -0,0 +1,7 @@
+## Define the function of the multiplication 
+def mul():
+    # Take the input form user and take the only two number of the integer 
+    a=int(input("Enter first number "))
+    b=int(input("Enter second number "))
+    ## THis is the show the result 
+    print(f"Multplication of {a} and {b} = {a*b}")
